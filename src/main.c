32f9b631020c12fc845e9917806bbf5d8cf0617d
Gabriel <biel755souza@gmail.com>
2026-01-24 16:24:45 -0300

    Add time measure to main()

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include "../headers/lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+   struct timespec start, end;
+   long long time_ns;
+
+   List* list = NULL;
+
+   clock_gettime(CLOCK_MONOTONIC, &start);
+
+
+      // Code to be measured
+      
+   
+   clock_gettime(CLOCK_MONOTONIC, &end);
+   
+   time_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+
+   printf("\nTime taken: %lld nanoseconds\n", time_ns);
+   printf("Time taken: %.6f milliseconds\n", time_ns / 1000000.0);
+
+   freeList(list);
+
+   return EXIT_SUCCESS;
+}
