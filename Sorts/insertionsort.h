4f7079c563a9ba3afae175731a8769e9f90ed683
Elilucio <elilucio.filho@aluno.ufca.edu.br>
2026-02-14 14:38:39 -0300

    Update insertionsort.h
    correção de importação para acesso em outra pasta

--- Sorts/insertionsort.h
+++ Sorts/insertionsort.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "listaestatica.h"
-#include "lists.h"
+#include "../headers/listaestatica.h"
+#include "../header/lists.h"
 
 void insertionSortE(ListaE *l) {
         int aux, j;
@@ -87,4 +87,5 @@ List: [30, 5, 2, 8, 1]
 List: [1, 2, 5, 8, 30]
 
 */
+
 
