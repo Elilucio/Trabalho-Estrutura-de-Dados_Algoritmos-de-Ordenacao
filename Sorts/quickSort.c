76bf831bde56ceb8903b0c54b85092826bdeb76d
sudo-invers <dhonatanjose8@gmail.com>
2026-02-15 13:09:09 -0300

    Por demanda da equipe, foi Refeito o quicksort, e adicionado mais comentarios, para ficar mais legivel

--- Sorts/quickSort.c
+++ Sorts/quickSort.c
@@ -1,87 +1,114 @@
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-void printArray(int *array, int size);
-void quickSort(int v[], int tamnaho);
+// protótipos das funções
+void quickSort(int v[], int n);
 
-int main() {
-  int VALUE = 5;
-  int inputed[VALUE];
-
-  srand(time(NULL));
-
-  for (int i = 0; i < VALUE; i++) {
-    inputed[i] = rand() % (VALUE + 1);
-  }
-
-  printf("Original array: ");
-  printArray(inputed, VALUE);
-
-  int tamanho = sizeof(inputed) / sizeof(inputed[0]);
-
-  quickSort(inputed, tamanho);
-
-  printf("Final array: ");
-  printArray(inputed, VALUE);
-
-  return EXIT_SUCCESS;
-}
-
-/*O(log n²*/
-void quickSort(int *v, int tamanho) {
-  int inicio = 0;
-  int fim = tamanho - 1;
-  int pilha[tamanho + 1];
-  int topo = -1;
-
-  int i, j, pivo, temp;
-
-  // empilha intervalo inicial
-  pilha[++topo] = inicio;
-  pilha[++topo] = fim;
-
-  while (topo >= 0) {
-    fim = pilha[topo--];
-    inicio = pilha[topo--];
-
-    pivo = v[fim];
-    i = inicio - 1;
-
-    for (j = inicio; j < fim; j++) {
-      if (v[j] <= pivo) {
-        i++;
-        temp = v[i];
-        v[i] = v[j];
-        v[j] = temp;
-      }
-    }
-
-    temp = v[i + 1];
-    v[i + 1] = v[fim];
-    v[fim] = temp;
-
-    int pos = i + 1;
-
-    // subvetor da esquerda
-    if (pos - 1 > inicio) {
-      pilha[++topo] = inicio;
-      pilha[++topo] = pos - 1;
-    }
-
-    // subvetor da direita
-    if (pos + 1 < fim) {
-      pilha[++topo] = pos + 1;
-      pilha[++topo] = fim;
-    }
-  }
-}
-
-void printArray(int *array, int size) {
-
-  for (int i = 0; i < size; i++) {
-    printf("%d, ", array[i]);
+// função auxiliar só para imprimir o vetor
+void imprimirVetor(int v[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d ", v[i]);
   }
   printf("\n");
+}
+/* // main de teste
+int main() {
+
+  int v[] = {34, 7, 23, 32, 5, 62, 15, 3};
+
+  int n = sizeof(v) / sizeof(v[0]);
+
+  printf("Vetor antes da ordenacao:\n");
+  imprimirVetor(v, n);
+
+  quickSort(v, n);
+
+  printf("\nVetor depois da ordenacao:\n");
+  imprimirVetor(v, n);
+
+  return 0;
+}
+*/
+
+/*
+ * Troca dois valores de lugar na memoria
+ * Os ponteiros alteram os valores originais do vetor
+ */
+void trocar(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+/*
+ * Funcao de particionamento do quick sort
+ *
+ * Escolhe o ultimo elemento como pivo
+ * Reorganiza o vetor para que:
+ * - todos os valores menores ou iguais ao pivô fiquem à esquerda
+ * - todos os valores maiores fiquem à direita
+ * Coloca o pivô na posição correta
+ * Retorna a posição final do pivô
+ */
+int particionar(int v[], int inicio, int fim) {
+  int pivo = v[fim];  // Escolhe o pivo (ultimo elemento do intervalo)
+  int i = inicio - 1; // 'i' marca o final da regiao dos valores menores que o
+                      // pivo Comeca antes do início do intervalo
+
+  for (int j = inicio; j < fim;
+       j++) { // 'j' percorre o intervalo comparando cada valor com o pivo
+    if (v[j] <= pivo) {     // Se o valor atual for menor ou igual ao pivo
+      i++;                  // Expande a regiao dos menores itens
+      trocar(&v[i], &v[j]); // Troca o valor atual para a regiao correta
+    }
+  }
+  trocar(&v[i + 1], &v[fim]); // Coloca o pivo logo apos a regiao dos menores,
+                              // essa sera a posicao definitiva do pivo
+  return i + 1;               // retorna a posicao final do pivo
+}
+
+/*
+ * Quicksort iterativo.
+ *
+ * Usa uma pilha manual para armazenar os intervalos do vetor
+ * que ainda precisam de ordenacao
+ */
+/*O(log n²*/
+void quickSort(int v[], int n) {
+  // pilha para armazenar pares (inicio, fim), cada intervalo ocupa duas
+  // posicoes
+  int pilha[n * 2];
+  int topo = 0;
+
+  // Empilha o intervalo inicial do vetor inteiro
+  pilha[topo++] = 0;
+  pilha[topo++] = n - 1;
+
+  while (topo > 0) {
+    // Desempilha o último intervalo inserido
+    int fim = pilha[--topo];
+    int inicio = pilha[--topo];
+
+    // Particiona o intervalo e obtém a posição do pivô
+    int posPivo = particionar(v, inicio, fim);
+
+    /*
+     * Se existirem elementos à esquerda do pivô,
+     * empilha esse novo subvetor para ordenação futura.
+     */
+
+    if (inicio < posPivo - 1) {
+      pilha[topo++] = inicio;
+      pilha[topo++] = posPivo - 1;
+    }
+
+    /*
+     * Se existirem elementos à direita do pivô,
+     * Empilha esse novo subvetor para ordenação futura.
+     */
+    if (posPivo + 1 < fim) {
+      pilha[topo++] = posPivo + 1;
+      pilha[topo++] = fim;
+    }
+  }
 }
